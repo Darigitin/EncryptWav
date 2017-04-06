6573f21c2dc360e67bb973ae84b9627e715a72e3
Matt G <Darigitin@hotmail.com>
2017-04-05 23:18:31 -0600

    Audio Recorder
    Started work on audio recorder implementation.

--- src/view/TriviumView.java
+++ src/view/TriviumView.java
@@ -2,25 +2,38 @@ package view;
 
 import controller.TriviumController;
 
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.util.Scanner;
+import java.io.IOException;
 
 public class TriviumView extends JFrame{
 
     private TriviumController controller;
-    private ButtonHandler handler;
-    JButton openButton;
-    JFileChooser chooser;
-    File file;
-    JButton submit;
+    private JButton openButton;
+    private JFileChooser chooser;
+    private File file, wavFile;
+    private JButton submit;
     private String publicKey;
     private String privateKey;
     private JTextField privateKeyField;
     private JTextField publicKeyField;
+    private JButton record;
+    private static final long RECORD_TIME = 10;
+    private AudioFileFormat.Type fileType = AudioFileFormat.Type.WAVE;
+    private TargetDataLine line;
+
+
+    private AudioFormat getAudioFormat(){
+        float sampleRate = 16000;
+        int sampleSizeInBits = 8;
+        int channels = 2;
+        return new AudioFormat(sampleRate, sampleSizeInBits,
+                channels, true, true);
+    }
 
 
     public TriviumView(TriviumController controller){
@@ -36,7 +49,7 @@ public class TriviumView extends JFrame{
 
     private void initComponents(){
        setLayout(new FlowLayout());
-       handler = new ButtonHandler();
+       ButtonHandler handler = new ButtonHandler();
        JPanel keyPanel = new JPanel();
        JLabel keyLabel = new JLabel("Keys:");
        keyPanel.add(keyLabel);
@@ -55,6 +68,9 @@ public class TriviumView extends JFrame{
        openButton = new JButton("Select a audio file");
        openButton.addActionListener(handler);
        keyPanel.add(openButton);
+       record = new JButton("Record an audio file");
+       record.addActionListener(handler);
+       keyPanel.add(record);
        submit = new JButton("Submit");
        keyPanel.add(submit);
        submit.addActionListener(handler);
@@ -77,6 +93,146 @@ public class TriviumView extends JFrame{
         return publicKey;
     }
 
+    private void startRecord(){
+        try{
+            AudioFormat format = getAudioFormat();
+            DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+            Mixer mixer = new Mixer() {
+                @Override
+                public Info getMixerInfo() {
+                    return null;
+                }
+
+                @Override
+                public Line.Info[] getSourceLineInfo() {
+                    return new Line.Info[0];
+                }
+
+                @Override
+                public Line.Info[] getTargetLineInfo() {
+                    return new Line.Info[0];
+                }
+
+                @Override
+                public Line.Info[] getSourceLineInfo(Line.Info info) {
+                    return new Line.Info[0];
+                }
+
+                @Override
+                public Line.Info[] getTargetLineInfo(Line.Info info) {
+                    return new Line.Info[0];
+                }
+
+                @Override
+                public boolean isLineSupported(Line.Info info) {
+                    return false;
+                }
+
+                @Override
+                public Line getLine(Line.Info info) throws LineUnavailableException {
+                    return null;
+                }
+
+                @Override
+                public int getMaxLines(Line.Info info) {
+                    return 0;
+                }
+
+                @Override
+                public Line[] getSourceLines() {
+                    return new Line[0];
+                }
+
+                @Override
+                public Line[] getTargetLines() {
+                    return new Line[0];
+                }
+
+                @Override
+                public void synchronize(Line[] lines, boolean b) {
+
+                }
+
+                @Override
+                public void unsynchronize(Line[] lines) {
+
+                }
+
+                @Override
+                public boolean isSynchronizationSupported(Line[] lines, boolean b) {
+                    return false;
+                }
+
+                @Override
+                public Line.Info getLineInfo() {
+                    return null;
+                }
+
+                @Override
+                public void open() throws LineUnavailableException {
+
+                }
+
+                @Override
+                public void close() {
+
+                }
+
+                @Override
+                public boolean isOpen() {
+                    return false;
+                }
+
+                @Override
+                public Control[] getControls() {
+                    return new Control[0];
+                }
+
+                @Override
+                public boolean isControlSupported(Control.Type type) {
+                    return false;
+                }
+
+                @Override
+                public Control getControl(Control.Type type) {
+                    return null;
+                }
+
+                @Override
+                public void addLineListener(LineListener lineListener) {
+
+                }
+
+                @Override
+                public void removeLineListener(LineListener lineListener) {
+
+                }
+            }
+
+            if (!AudioSystem.isLineSupported(info)){
+                System.out.println("Line not supported");
+                System.exit(0);
+            }
+            line = (TargetDataLine) AudioSystem.getLine(info);
+            System.out.println(line.toString());
+            line.open(format);
+            line.start();
+            System.out.println("Start capturing...");
+            AudioInputStream ais = new AudioInputStream(line);
+            System.out.println("Start recording...");
+            AudioSystem.write(ais, fileType, wavFile);
+        }
+        catch(LineUnavailableException | IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    private void finish(){
+        line.stop();
+        line.close();
+        System.out.println("Finished");
+    }
+
     private class ButtonHandler implements ActionListener{
 
         @Override
@@ -88,10 +244,23 @@ public class TriviumView extends JFrame{
                     file = chooser.getSelectedFile();
                 }
             }
-            if (source == submit){
+            else if (source == submit){
                 privateKey = privateKeyField.getText();
                 publicKey = publicKeyField.getText();
                 controller.run();
+            }
+            else if (source == record){
+                wavFile = new File("RecordAudio.wav");
+                Thread stopper = new Thread(() -> {
+                    try {
+                        Thread.sleep(RECORD_TIME);
+                    }catch (InterruptedException ex){
+                        ex.printStackTrace();
+                    }
+                    finish();
+                });
+                stopper.start();
+                startRecord();
             }
         }
     }
